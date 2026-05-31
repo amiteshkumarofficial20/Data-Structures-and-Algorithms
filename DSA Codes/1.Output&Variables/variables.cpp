69b6088a654ef9a7943265796a69c89b0b3435e5
amiteshkumarofficial20 <amiteshkumarofficial.20@gmail.com>
2026-05-31 11:36:02 +0530

    Output and Variables

---
+++ DSA Codes/1.Output&Variables/variables.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // variables as containers
+    int x;
+    x = 7;
+    cout << x << endl;
+    x = 17;
+    cout << x << endl;
+    x = 27;
+    x = 11;
+    cout << x << endl;
+    x = x + 6;
+    cout << x << endl;
+    x -= 20;
+    cout << x << endl;
+    int y = 19;
+    cout << y << endl;
+    return 0; // optional
+}
